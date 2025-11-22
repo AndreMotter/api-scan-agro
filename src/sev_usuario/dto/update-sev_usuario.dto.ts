740ecdb23655ea5da440a8495aaf01c9023e6c78
AndreMotter <andremotter09@gmail.com>
2025-11-22 00:58:40 -0300

    ajuste

--- src/sev_usuario/dto/update-sev_usuario.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSevUsuarioDto } from './create-sev_usuario.dto';
-
-export class UpdateSevUsuarioDto extends PartialType(CreateSevUsuarioDto) {}
--- src/sev_usuario/sev_usuario.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SevUsuarioService } from './sev_usuario.service';
-import { SevUsuarioController } from './sev_usuario.controller';
-
-@Module({
-  controllers: [SevUsuarioController],
-  providers: [SevUsuarioService],
-})
-export class SevUsuarioModule {}

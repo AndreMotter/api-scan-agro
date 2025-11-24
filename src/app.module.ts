b69f96b2ae2cb881d425dc5e98c9d03e45e9c3df
AndreMotter <andremotter09@gmail.com>
2025-11-24 20:24:31 -0300

    ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { SgrCulturaModule } from './sgr_cultura/sgr_cultura.module';
 import { SgrUsuarioModule } from './sgr_usuario/sgr_usuario.module';
-import { SevUsuarioModule } from './sev_usuario/sev_usuario.module';
 import { SrhLeituraModule } from './srh_leitura/srh_leitura.module';
 
 @Module({
-  imports: [SgrCulturaModule, SgrUsuarioModule, PrismaModule, SevUsuarioModule, SrhLeituraModule],
+  imports: [SgrCulturaModule, SgrUsuarioModule, SrhLeituraModule, PrismaModule],
   controllers: [],
   providers: [],
 })
